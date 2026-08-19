3181251ac6a282b7cbdd88e81a9ccca469e7ce99
hexiangliangtai-arch <hexiangliangtai@gmail.com>
2026-08-19 18:42:36 +0900

    ヘッダーの追加

---
+++ front/src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="relative mx-auto h-20 w-full max-w-[402px] shrink-0 overflow-hidden border-b border-solid border-[#00000033] bg-[#FBCFE8]">
+      <Image
+        src="/header/kyunpass-icon.svg"
+        alt=""
+        width={40}
+        height={40}
+        unoptimized
+        className="absolute left-[18px] top-5 size-10"
+      />
+
+      <span className="absolute left-[75.962px] top-[30.672px] h-[22.849px] w-[113.959px]">
+        <Image
+          src="/header/kyunpass-logo.svg"
+          alt="きゅんぱす"
+          width={116}
+          height={25}
+          unoptimized
+          className="absolute -left-px -top-px h-[24.849px] w-[115.959px] max-w-none"
+        />
+      </span>
+    </header>
+  );
+}
