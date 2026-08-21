805cca3c1c0a2a6b446c5972c0a3badc0195997b
Yuuuki16 <youximasheng83@gmail.com>
2026-08-21 23:25:04 +0900

    変更

--- front/src/components/ScoreBadge.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
-import { ScoreBadge } from "./ScoreBadge";
-
-test("表示されるスコアがそのまま描画される", () => {
-  render(<ScoreBadge score={72} />);
-  expect(screen.getByText("キュン度 72")).toBeInTheDocument();
-});
